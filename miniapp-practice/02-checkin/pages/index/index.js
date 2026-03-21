712a2195f6299d5eb6ff464c1514ddca9b76d17e
peterzhan321 <2164934458@qq.com>
2026-03-21 14:26:51 +0800

    feat(打卡页面): 实现打卡记录器的基本界面和功能
    添加打卡记录器的基本数据结构，包括记录列表和输入框
    实现页面布局和样式，包括统计区域、输入区域和记录列表
    添加输入处理和打卡功能，能够记录打卡内容和时间
    更新TODO列表标记任务1为已完成

--- miniapp-practice/02-checkin/pages/index/index.js
+++ miniapp-practice/02-checkin/pages/index/index.js
@@ -1,2 +1,24 @@
 // index.js
-Page({})
+Page({
+  data: {
+    records: [],
+    inputValue: ''
+  },
+
+  onLoad() {
+  },
+
+  onInputChange(e) {
+    this.setData({
+      inputValue: e.detail.value
+    })
+  },
+
+  onCheckin() {
+    this.data.records.push({
+      content: this.data.inputValue,
+      time: new Date().toLocaleString()
+    });
+    console.log('records 长度：', this.data.records.length);
+  }
+})

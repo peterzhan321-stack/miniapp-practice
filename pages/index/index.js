f4a5f63d7f62354d37eb4d04d6a72e8d5c3c7083
peterzhan321 <2164934458@qq.com>
2026-03-21 14:42:20 +0800

    feat: 添加空状态提示和删除功能

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,4 +1,6 @@
 // index.js
+const STORAGE_KEY = 'checkin_records'
+
 Page({
   data: {
     records: [],
@@ -6,14 +8,33 @@ Page({
   },
 
   onLoad() {
+    const records = wx.getStorageSync(STORAGE_KEY) || []
+    this.setData({ records })
   },
 
-  onInputChange(e) {
+  onInput(e) {
     this.setData({
       inputValue: e.detail.value
     })
   },
 
   onCheckin() {
+    const newRecord = {
+      content: this.data.inputValue,
+      time: new Date().toLocaleString()
+    };
+    const records = [newRecord, ...this.data.records]
+    this.setData({
+      records,
+      inputValue: ''
+    });
+    wx.setStorageSync(STORAGE_KEY, records)
+  },
+
+  onDelete(e) {
+    const index = e.currentTarget.dataset.index
+    const records = this.data.records.filter((_, i) => i !== index)
+    this.setData({ records })
+    wx.setStorageSync(STORAGE_KEY, records)
   }
 })
